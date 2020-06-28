9ef0ad25addeb1d314ba8304203ac57636fd36c0
Nistorvski <58191851+Nistorvski@users.noreply.github.com>
2020-06-28 11:05:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 8077;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/garden', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect('mongodb://134.209.240.40:27017/garden', { useUnifiedTopology: true, useNewUrlParser: true })
     .then(()=>{
         console.log("Conexión a la base de datos establecida con exito..¡!");
 
