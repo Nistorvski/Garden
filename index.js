d34da87c7f90fc53633f62ac6c22e4dd4f580c49
Nistorvski <58191851+Nistorvski@users.noreply.github.com>
2020-06-28 12:46:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 8077;
+var appex = require('express');
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -10,7 +11,7 @@ mongoose.connect('mongodb://localhost:27017/garden', { useUnifiedTopology: true,
         console.log("Conexión a la base de datos establecida con exito..¡!");
 
         //settings 
-        app.set(port, process.env.PORT || 3000);
+        appex.set(port, process.env.PORT || 3000);
 
         //Crear el servidor
         app.listen(port, ()=>{
